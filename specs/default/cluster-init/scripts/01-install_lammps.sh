cd8f1f4cd9e1f79258f2e947c2eaf69a529b37f4
Dan Harris <dan.harris@microsoft.com>
2020-01-09 10:48:09 -0700

    cuser.home_dir is deprecated, use the homedir of the cluster owner instead
    Fixes #3

--- specs/default/cluster-init/scripts/01-install_lammps.sh
+++ specs/default/cluster-init/scripts/01-install_lammps.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-HOMEDIR=$(jetpack config cuser.home_dir)
 CLUSTER_OWNER=$( jetpack config cyclecloud.cluster.user.name ) 
+HOMEDIR=$( getent passwd "$CLUSTER_OWNER" | cut -d: -f6 )
 
 set -ex
 # Don't run if we've already expanded the LAMMPS tarball
-if [ -d /shared/scratch/lammps ]; then
-    exit 0
-fi
-LAMMPS_VERSION=$(jetpack config lammps.version)
+if [ ! -d /shared/scratch/lammps ]; then
+    LAMMPS_VERSION=$(jetpack config lammps.version)
 
-pushd /shared/scratch
-jetpack download "lammps-${LAMMPS_VERSION}.tgz"
-tar xvzf lammps-${LAMMPS_VERSION}.tgz
-popd
+    pushd /shared/scratch
+    jetpack download "lammps-${LAMMPS_VERSION}.tgz"
+    tar xvzf lammps-${LAMMPS_VERSION}.tgz
+    popd
+fi
 
 cp ${CYCLECLOUD_SPEC_PATH}/files/run_lammps.sh ${HOMEDIR}
 cp ${CYCLECLOUD_SPEC_PATH}/files/run_lammps_mpi.sh ${HOMEDIR}
