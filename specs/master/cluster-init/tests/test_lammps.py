e56668fd1f695134852945c6d0f4ed9953c1f8e3
Ryan Hamel <ryan.hamel@cyclecomputing.com>
2020-01-09 15:41:16 -0500

    Run a simple blocking qsub as an integration test

--- specs/master/cluster-init/tests/test_lammps.py
+++ specs/master/cluster-init/tests/test_lammps.py
@@ -1,20 +1,27 @@
 #!/opt/cycle/jetpack/system/embedded/bin/python -m pytest
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+import jetpack.config
 import os
-import glob
 import subprocess
+import tempfile
+import time
 
 
 def test_lammps():
-    # Change directory to lammps scripts directory
-    os.chdir('/mnt/cluster-init/lammps/default/files')
-
-    # Make shell scripts executable
-    shell_files = glob.glob('*.sh')
-    for script in shell_files:
-        os.chmod(os.path.join('.', script), 0755)
-
-    # run_lammps_mpi.sh requires specific drivers, but
-    # run_lammps.sh should run on any machine.
-    subprocess.check_call([os.path.join('.', 'run_lammps.sh')])
+    cluster_owner = str(jetpack.config.get("cyclecloud.cluster.user.name"))
+    script_path = os.path.expanduser("~%s/run_lammps.sh" % cluster_owner)
+    stdout = os.path.expanduser("~%s/%s.test.out" % (cluster_owner, time.time()))
+    
+    try:
+        subprocess.check_call(["sudo", "-u", cluster_owner, "qsub", "-Wblock=true", "-l", "select=2", "-o", stdout, "--", script_path])
+        # The results may take a bit to get copied back. Give it up to a minute and make sure it is not empty.
+        for _ in range(60):
+            if os.path.exists(stdout):
+                assert os.path.getsize(stdout) > 0
+                return
+            time.sleep(1)
+        raise AssertionError("Could not get results! {}".format(stdout))
+    finally:
+        if os.path.exists(stdout):
+            os.remove(stdout)
